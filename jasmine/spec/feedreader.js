11836e4324a11e0341708872af09a11c82bb4806
Chilja Speransky <magiclanzelot@gmx.net>
2018-04-16 15:28:59 +0200

    last 2 tests changed to meet the specs
    async beforeEach changed, usage of the callback function of loadFeed() now

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,93 +1,87 @@
 $(function() {
-		describe('RSS Feeds', function() {
+	describe('RSS Feeds', function() {
+		it('are defined', function() {
+			expect(allFeeds).toBeDefined();
+			expect(allFeeds.length).not.toBe(0);
+		});
 
-				it('are defined', function() {
-					expect(allFeeds).toBeDefined();
-					expect(allFeeds.length).not.toBe(0);
+		it('have URL', function() {
+			for(let i = 0; i < allFeeds.length; i++) {
+				const object = allFeeds[i];
+				const objectLength = object.url.length;
+
+				expect(objectLength).toBeGreaterThan(0);
+			}
+		});
+
+		it('have name', function() {
+			for(let i = 0; i < allFeeds.length; i++) {
+				const object = allFeeds[i];
+				const objectLength = object.name.length;
+
+				expect(object.name).toBeDefined();
+				expect(objectLength).toBeGreaterThan(0);
+			}
+		});
+	});
+
+	describe('The menu', function() {
+			 it('is hidden by default', function() {
+			 		const menuClassName = $('body').hasClass('menu-hidden');
+
+					expect(menuClassName).toBe(true);
+			 });
+
+				/* check the class of th ebody after a click on the menu icon
+				 * and recheck after another click
+				*/
+				it('check visibility when clicked', function() {
+					const menu = document.querySelector('.menu-icon-link');
+
+					menu.click();
+					const newMenuClass = $('body').hasClass('menu-hidden');
+					expect(newMenuClass).toBe(false);
+
+					menu.click();
+					const menuClass = $('body').hasClass('menu-hidden');
+					expect(menuClass).toBe(true);
 				});
+	});
 
-				it('have URL', function() {
-					for(let i = 0; i < allFeeds.length; i++) {
-						const object = allFeeds[i];
-						const objectLength = object.url.length;
+	describe('Initial Entries', function() {
+		/* beforeEach for asynchronous function loadFeed(), second argument is a callback
+		*/
+		beforeEach(function(done){
+			jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000;
+			loadFeed(0, done);
+		});
 
-						expect(objectLength).toBeGreaterThan(0);
-					}
+		// check the length of container (number of childs)
+		it('at least one .entry element in the .feed container', function() {
+			const feedContainer = $('.feed').children().length;
+			expect(feedContainer).toBeGreaterThan(0);
+		});
+	});
+
+	describe('New Feed Selection', function() {
+		let feed, otherFeed;
+		 /* beforeEach for asynchronous function loadFeed(), second argument is a callback
+		*/
+		beforeEach(function(done){
+			jasmine.DEFAULT_TIMEOUT_INTERVAL = 2000;
+
+			loadFeed(0, function() {
+				feed = $('.feed h2').last().text();
+
+				loadFeed(1, function() {
+					otherFeed = $('.feed h2').last().text();
 				});
-
-				it('have name', function() {
-					for(let i = 0; i < allFeeds.length; i++) {
-						const object = allFeeds[i];
-						const objectLength = object.name.length;
-
-						expect(object.name).toBeDefined();
-						expect(objectLength).toBeGreaterThan(0);
-					}
-				});
-
-		});
-
-		describe('The menu', function() {
-
-				 it('is hidden by default', function() {
-				 		const menuClassName = $('body').hasClass('menu-hidden');
-
-						expect(menuClassName).toBe(true);
-				 });
-
-					/* check the class of th ebody after a click on the menu icon
-					 * and recheck after another click
-					*/
-					it('check visibility when clicked', function() {
-						const menu = document.querySelector('.menu-icon-link');
-
-						menu.click();
-						const newMenuClass = $('body').hasClass('menu-hidden');
-						expect(newMenuClass).toBe(false);
-
-						menu.click();
-						const menuClass = $('body').hasClass('menu-hidden');
-						expect(menuClass).toBe(true);
-					});
-		});
-
-		describe('Initial Entries', function() {
-
-			/* beforeEach for asynchronous functions, wait for init() because it starts the
-			 * execution of loadFeed()
-			*/
-			beforeEach(function(done){
-				jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000;
-				init();
-				done();
-			});
-
-			// check the length of container (number of childs)
-			it('at least one .entry element in the .feed container', function() {
-				const container = $('.feed');
-				expect(container.length).toBeGreaterThan(0);
+				done()
 			});
 		});
 
-		describe('New Feed Selection', function() {
-
-			 /* the feed class is compared before and after running loadFeed() through
-			 *	asynchronous beforeEach it (loadFeed() is stored in init())
-			 */
-			 const feed = $('.feed');
-			 console.log(feed);
-
-			 beforeEach(function(done){
-				jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000;
-				init();
-				done();
-			});
-
-			it('feed content changes after the loadFeed function is executed', function() {
-				const newFeed = $('.feed');
-				console.log(newFeed);
-
-				expect(newFeed).not.toBe(feed);
-			});
+		it('feed content changes after the loadFeed function is executed', function() {
+			expect(otherFeed).not.toBe(feed);
 		});
+	});
 }());
